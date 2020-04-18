57504123103599406b1de9f1423ef1d4ffdf7336
Jamile Lima <jamilelima@protonmail.com>
2020-04-18 00:14:52 -0300

    chore: add colors

---
+++ src/utils/colors.js
@@ -0,0 +1,17 @@
+const colors = {
+  primary: '#171B1E',
+  accent: '#535AF6',
+  davy_gre: '#1B232F',
+  cool_grey: '#90949c',
+  prince_ton_orange: '#f99500',
+  white: '#FFF',
+  red: '#ff0000',
+  manatee: '#979797',
+  onyx: '#101010',
+  white_15: '#26ffffff',
+  white_50: '#80FFFFFF',
+  orange: '#F76353',
+  green: '#00CC99',
+  yellow: '#F2B900',
+};
+export default colors;
