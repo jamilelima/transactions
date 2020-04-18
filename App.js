0e431c4f100c23d8068a2f5b5d8cb371c6077765
Jamile Lima <jamilelima@protonmail.com>
2020-04-18 18:07:16 -0300

    chore: setup navigation

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import Home from './src/screens/home/index.js';
+import CreateTransaction from './src/screens/createTransaction/index.js';
 
-const App: () => React$Node = () => {
-  return <Home />;
-};
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="CreateTransaction" component={CreateTransaction} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
