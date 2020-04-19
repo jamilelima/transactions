5f0c6e11762722d386dbee34399e4ecd081d377a
Jamile Lima <jamilelima@protonmail.com>
2020-04-19 17:36:47 -0300

    chore: add logic to show icon

--- src/components/transactionItem/index.js
+++ src/components/transactionItem/index.js
@@ -8,22 +8,25 @@ import {
 import Icon from 'react-native-vector-icons/Feather';
 import {View} from 'react-native';
 import colors from '../../utils/colors';
+import {convertForReal} from '../../utils/utils';
 
 export default function TransactionItem({data}) {
+  const _setupIcon = () => {
+    if (data.type === 'credit') {
+      return <Icon name="arrow-up-right" color={colors.green} size={24} />;
+    } else {
+      return <Icon name="arrow-down-right" color={colors.orange} size={24} />;
+    }
+  };
+
   return (
     <Container>
-      <Icon
-        name="arrow-up-right"
-        color={data.isRecive ? colors.green : colors.green}
-        size={24}
-      />
-
+      {_setupIcon()}
       <View>
         <Description>{data.description}</Description>
-        <TransactionDate>20/04/19</TransactionDate>
+        <TransactionDate>{data.date}</TransactionDate>
       </View>
-
-      <TransactionValue> +R$ 300,00</TransactionValue>
+      <TransactionValue> +R$ {convertForReal(data.value)}</TransactionValue>
     </Container>
   );
 }
