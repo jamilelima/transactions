e8c2b8f591a8ee6ee9f19d36ad52d8c8ef610ca8
Jamile Lima <jamilelima@protonmail.com>
2020-04-20 11:00:15 -0300

    chore: add loading component

---
+++ src/components/loading/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+import colors from '../../utils/colors';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 60px;
+  background-color: ${colors.davy_gre};
+`;
