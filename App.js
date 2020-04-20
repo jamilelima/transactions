3a4e1e25e473b783e79a83fa1078ee7f70b393d3
Jamile Lima <jamilelima@protonmail.com>
2020-04-19 22:09:19 -0300

    chore: remove appbar

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import Home from './src/screens/home/index.js';
 import CreateTransaction from './src/screens/createTransaction/index.js';
+import colors from './src/utils/colors.js';
 
 const Stack = createStackNavigator();
 
@@ -12,8 +13,20 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="CreateTransaction" component={CreateTransaction} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="CreateTransaction"
+          component={CreateTransaction}
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
