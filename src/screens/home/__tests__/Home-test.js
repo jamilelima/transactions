7116c40c7d1147bb6980cf4b22e94755760902c3
Jamile Lima <jamilelima@protonmail.com>
2020-04-20 22:51:35 -0300

    chore: setup home test

---
+++ src/screens/home/__tests__/Home-test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {create} from 'react-test-renderer';
+import {shallow} from 'enzyme';
+import mockData from '../../../utils/mockData/mockData';
+import Home from '../../home';
+
+describe('Test Home component', () => {
+  let component;
+  let defaultState = {
+    transactions: [],
+    newBalance: 0,
+  };
+
+  test('should render component', () => {
+    const snapshot = create(<Home />).toJSON();
+    expect(snapshot).toMatchSnapshot();
+  });
+
+  beforeEach(() => {
+    component = shallow(<Home />);
+  });
+
+  it('has default state', () => {
+    expect(component.state()).toEqual(defaultState);
+  });
+});
