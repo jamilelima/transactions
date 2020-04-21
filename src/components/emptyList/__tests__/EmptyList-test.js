aaf307c585f7691ac4747c71bae0a25c41655e0f
Jamile Lima <jamilelima@protonmail.com>
2020-04-20 22:51:46 -0300

    chore: setup empty list

---
+++ src/components/emptyList/__tests__/EmptyList-test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import EmptyList from '../index';
+import {create} from 'react-test-renderer';
+import {shallow} from 'enzyme';
+import 'jest-styled-components';
+
+describe('Test EmptyList component', () => {
+  const emptyList = create(<EmptyList />);
+  test('Match with snapashot', () => {
+    expect(emptyList.toJSON()).toMatchSnapshot();
+  });
+  test('Test empty list text', () => {
+    const component = shallow(<EmptyList />);
+    expect(
+      component.text().includes('A lista está vazia. Adicione uma transação.'),
+    ).toBe(true);
+  });
+  test("Test empty list text for a test that doesn't exist", () => {
+    const component = shallow(<EmptyList />);
+    expect(
+      component.text().includes('A lista está vazia. Adicione uma transação.'),
+    ).toBe(true);
+  });
+});
